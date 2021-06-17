8c08d6f87f42aa6846199d8df3cfdea04b2a8af0
justsoph <sophiaanaklumythic@gmail.com>
2021-06-18 06:47:24 +0900

    Atualização

--- www/lib/script.js
+++ www/lib/script.js
@@ -1,16 +1,16 @@
 window.onload = function(){
   const buscar = document.querySelector("#buscar");
-
+  const cep = document.querySelector("#cep");
   const opcoes = {
     method: `GET`,
     mode: `cors`,
     cache: `default`
   }
   buscar.addEventListener("click", function(){
-    fetch(`https://viacep.com.br/ws/01001000/json/`, opcoes)
+    fetch(`https://viacep.com.br/ws/${ cep.value }/json/`, opcoes)
       .then(response => {response.json()
         .then(data => {
-          alert(`CEP: ` + data[`cep`]);
+          document.querySelector("#cep").value = data['uf', 'localidade', 'logradouro'];
         })
       })
   });
